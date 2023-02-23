4b19689722a030c0ab2746f38fc1b0c9ddb524fd
DADJOD <kamu57@mail.ru>
2023-02-23 15:36:56 +0300

    commit_5
    commit_5

--- app/src/main/java/com/example/opensecondactivity/AirplaneModeReceiver.kt
+++ app/src/main/java/com/example/opensecondactivity/AirplaneModeReceiver.kt
@@ -1,18 +1,42 @@
 package com.example.opensecondactivity
 
 import android.annotation.SuppressLint
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class AirplaneModeReceiver : BroadcastReceiver() {
-    @SuppressLint("UnsafeProtectedBroadcastReceiver")
+    @SuppressLint("UnsafeProtectedBroadcastReceiver", "MissingPermission",
+        "UnspecifiedImmutableFlag"
+    )
     override fun onReceive(context: Context?, intent: Intent?) {
-        Log.d("happySDK", "AirplaneModeReceiver")
+        if (intent != null && !intent.getBooleanExtra("state", false)) {
+            Log.d("happySDK", "AirplaneModeReceiver")
 
-        val newIntent = Intent(context, MyService::class.java)
-        newIntent.putExtra(MyService.SERVICE_DATA_EXTRA, "AirplaneModeReceiver")
-        context?.startService(newIntent)
+            val google = Intent(Intent.ACTION_VIEW)
+            val uri = Uri.parse("http://google.com")
+            google.data = uri
+
+            val pendingIntent = PendingIntent.getActivity(context, 1, google, 0)
+
+            val builder = NotificationCompat.Builder(context!!, NOTIFICATION_CHANNEL_ID2)
+                .setContentTitle("Google")
+                .setContentText("It's time to go Google")
+                .setSmallIcon(android.R.drawable.btn_radio)
+                .setContentIntent(pendingIntent)
+
+
+
+            NotificationManagerCompat.from(context).notify(1, builder.build())
+        }
+    }
+
+    companion object {
+        const val NOTIFICATION_CHANNEL_ID2 = "my_channel_id_02"
     }
 }
